acc242c2cce7702d53d52e10637b2b2e6b4954cf
cryptocat <tkiq0725@gmail.com>
2019-07-23 00:24:42 +0900

    不要なライブラリインポートを削除

--- LineNotifyBot/LineNotifyBot.py
+++ LineNotifyBot/LineNotifyBot.py
@@ -6,13 +6,7 @@ import calendar
 import ccxt
 import ccxtWrapper
 import math
-import matplotlib.pyplot as plt
 import io
-
-from pyti.exponential_moving_average import exponential_moving_average as ema
-from pyti.moving_average_convergence_divergence import moving_average_convergence_divergence as macd
-from pyti.relative_strength_index import relative_strength_index as rsi
-
 
 import numpy
 import orderManager
